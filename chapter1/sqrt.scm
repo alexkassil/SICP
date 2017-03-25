65b77c9c59ce944d1658e93f1c55d5f64f42c241
Alex Kassil <contactalexkassil@gmail.com>
2017-03-25 12:26:01 -0700

    Created better square root function
    The better square root function, called better-sqrt, check for changes
    between iterations for better accuracy at the very low numbers

--- chapter1/sqrt.scm
+++ chapter1/sqrt.scm
@@ -1,4 +1,5 @@
 (define (sqrt-iter guess x)
+  (display (- (square (improve guess x)) x)) (newline)
   (if (good-enough? guess x)
       guess
       (sqrt-iter (improve guess x)
@@ -15,3 +16,16 @@
 
 (define (sqrt x)
   (sqrt-iter 1.0 x))
+
+(define (better-good-enough? guess x)
+  (< (abs (- (square (improve guess x)) x)) 1.0e-8))
+
+(define (better-sqrt-iter guess x)
+  (display (- (square (improve guess x)) x)) (newline)
+  (if (better-good-enough? guess x)
+      guess
+      (better-sqrt-iter (improve guess x)
+		 x)))
+
+(define (better-sqrt x)
+  (better-sqrt-iter 1.0 x))
