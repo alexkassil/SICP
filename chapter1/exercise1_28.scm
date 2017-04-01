edb13726a40d5eda644396affee59bc54dc7c45b
Alex Kassil <contactalexkassil@gmail.com>
2017-04-01 12:45:43 -0700

    Completed slow Miller-Rabin prime test

--- chapter1/exercise1_28.scm
+++ chapter1/exercise1_28.scm
@@ -11,6 +11,16 @@
 (define (fermat a n)
   (remainder (- (expt a (- n 1)) 1) n))
 
+(define (fermat-test n)
+  (define (try-it a)
+    (= (fermat a n) 0))
+  (try-it (+ 2 (random (- n 2)))))
+
+(define (slow-prime? n times)
+  (cond ((= times 0) true)
+	((fermat-test n) (slow-prime? n (- times 1)))
+	(else false)))
+
 
 
 
