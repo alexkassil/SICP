d41bbb37f685baded0ae53dfcb98bed2a101b8ed
Alex Kassil <contactalexkassil@gmail.com>
2017-03-31 23:06:21 -0700

    Finished exercise 24, fixed rounding error

--- chapter1/exercise1_22.scm
+++ chapter1/exercise1_22.scm
@@ -5,7 +5,7 @@
   (display n)
   (start-prime-test n (runtime)))
 (define (start-prime-test n start-time)
-  (if (prime? n)
+  (if (fast-prime? n 1000)
       (report-prime (- (runtime) start-time))))
 (define (report-prime elapsed-time)
   (display " *** ")
@@ -37,6 +37,56 @@
 (define (search-for-primes start n)
   (timed-prime-test start)
   (cond ((> n 0) (search-for-primes (+ start 2) (- n 1)))))
+
+
+;;Fast prime function
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+	((even? exp)
+	 (remainder (square (expmod base (/ exp 2) m))
+		    m))
+	(else
+	 (remainder (* base (expmod base (- exp 1) m))
+		    m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (round (random (- n 1))))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+	((fermat-test n) (fast-prime? n (- times 1)))
+	(else false)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
