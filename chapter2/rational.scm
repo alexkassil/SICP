d025b69c36cbe55078db57bbbb6d362433daa51e
Alex Kassil <contactalexkassil@gmail.com>
2017-04-08 15:32:53 -0700

    Added division and equality

--- chapter2/rational.scm
+++ chapter2/rational.scm
@@ -31,6 +31,18 @@
 	    (* (denom x) (denom y))))
 
 ;; Multiplication
-(define (mult-rat x y)
+(define (mul-rat x y)
   (make-rat (* (numer x) (numer y))
 	    (* (denom x) (denom y))))
+
+;; Division
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+	    (* (denom x) (numer y))))
+
+;;; Other useful functions
+
+;; Equality
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y)) (* (numer y) (denom x))))
