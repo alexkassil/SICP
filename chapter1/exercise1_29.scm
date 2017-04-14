3e9fb9bcf2731fb2b8de1ebd919e41340d9996d4
Alex Kassil <contactalexkassil@gmail.com>
2017-04-13 17:00:31 -0700

    Finished Simsons Rule

--- chapter1/exercise1_29.scm
+++ chapter1/exercise1_29.scm
@@ -1,6 +1,6 @@
 ;;;; Simpson's Rule
 
-;; Sigma
+;; Simpson's Rule Sum
 
 (define (sum term a next b)
   (if (> a b)
@@ -8,11 +8,22 @@
       (+ (term a)
 	 (sum term (next a) next b))))
 
+;; Increment
+
+(define (inc x) (+ x 1))
+
 ;; Integral using Simpson's Rule
 
 (define (simpson-integral f a b n)
   (define h (/ (- b a) n))
-  (display h))
+  (define (yk k) (f (+ a (* k h))))
+  (define (simpson-term k)
+    (* (cond ((or (= k 0) (= k n)) 1)
+	     ((odd? k) 4)
+	     (else 2))
+       (yk k)))
+  (* (/ h 3.0)
+     (sum simpson-term 0 inc n)))
 
 
 
