2f2a367a5116a90c5701ba89c1fb28e75330cc73
Alex Kassil <contactalexkassil@gmail.com>
2017-04-16 21:01:27 -0700

    Started Exercise 1.36 - displaying fixed-point approximations

---
+++ chapter1/exercise1_36.scm
@@ -0,0 +1,16 @@
+;;;; Fixed Point
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (display next)
+      (newline)
+      (if (close-enough? guess next)
+	  next
+	  (try next))))
+  (try first-guess))
