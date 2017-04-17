76a1780f1e4aba69170475ec479ee22adb4fa286
Alex Kassil <contactalexkassil@gmail.com>
2017-04-17 12:27:23 -0700

    Added helper functions

--- chapter1/exercise1_45.scm
+++ chapter1/exercise1_45.scm
@@ -1,10 +1,5 @@
 ;;;; nth roots computer
 
-(define (repeated f times)
-  (if (< times 1)
-      (lambda (x) x)
-      (compose f (repeated f (- times 1)))))
-
 (define (n-root n)
   (lambda (x)
     (fixed-point
@@ -16,7 +11,27 @@
 	(/ x (expt y (- n 1)))))
      1.0)))
 
+;;; Helper functions
 
+(define (average x y)
+  (/ (+ x y) 2))
 
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
 
+(define (repeated f times)
+  (if (< times 1)
+      (lambda (x) x)
+      (compose f (repeated f (- times 1)))))
 
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+	  next
+	  (try next))))
+  (try first-guess))
