ee2f31be65ecd671175fbaee2afaf87bdd9f01c4
Alex Kassil <contactalexkassil@gmail.com>
2017-04-16 21:26:24 -0700

    Fixed to add 1/phi and phi approximations

--- chapter1/exercise1_37.scm
+++ chapter1/exercise1_37.scm
@@ -12,7 +12,11 @@
       (/ (N k) (+ (D k)
 		  (cont-frac N D (- k 1))))))
 
-; Approximates phi to 4 decimal places (1.6180)
+; Approximates 1/phi to 4 decimal places (1.6180)
 (cont-frac (lambda (i) 1.0)
 	   (lambda (i) 1.0)
 	   9)
+
+(/ 1 (cont-frac (lambda (i) 1.0)
+		(lambda (i) 1.0)
+		11))
