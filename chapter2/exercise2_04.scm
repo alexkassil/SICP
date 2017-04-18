4b983cb9d15a10b4e5654e436d45f17b46413e07
Alex Kassil <contactalexkassil@gmail.com>
2017-04-18 14:39:23 -0700

    Exercise 2.04 cons, car, and cdr

---
+++ chapter2/exercise2_04.scm
@@ -0,0 +1,10 @@
+;;;; cons, car, and cdr as a procedural representation
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
