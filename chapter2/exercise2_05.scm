d537520e52f5553553dc855344b3dddb712cf5f0
Alex Kassil <contactalexkassil@gmail.com>
2017-04-18 14:50:38 -0700

    Exercise 2.05 representing pairs as a number

---
+++ chapter2/exercise2_05.scm
@@ -0,0 +1,18 @@
+;;;; cons, car, and cdr as one number
+
+(define (cons a b)
+  (* (expt 2 a) (expt 3 b)))
+
+(define (car z)
+  (define (2-iter n current)
+    (if (= 1 (modulo current 2))
+	n
+	(2-iter (+ n 1) (/ current 2))))
+  (2-iter 0 z))
+
+(define (cdr z)
+  (define (3-iter n current)
+    (if (not (= 0 (modulo current 3)))
+	n
+	(3-iter (+ n 1) (/ current 3))))
+  (3-iter 0 z))
