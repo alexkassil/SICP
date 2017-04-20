5700da6fdade1a3f98a6a42780125f0253210f07
Alex Kassil <contactalexkassil@gmail.com>
2017-04-19 23:34:32 -0700

    Changes subtract-interval to use addition

--- chapter2/exercise2_08.scm
+++ chapter2/exercise2_08.scm
@@ -1,7 +1,19 @@
 ;;;; Interval Subtract
 
-(define (subtract-interval x y)
-  (make-interval (- (lower-bound x)
+(define (make-interval a b) (cons a b))
+
+(define (lower-bound x) (min (car x) (cdr x)))
+
+(define (upper-bound x) (max (car x) (cdr x)))
+
+(define (sub-interval x y)
+  (add-interval x 
+		(make-interval
+		 (- (upper-bound y)) 
+		 (- (lower-bound y)))))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x)
 		    (lower-bound y))
-		 (- (upper-bound x)
+		 (+ (upper-bound x)
 		    (upper-bound y))))
