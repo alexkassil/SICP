25d6f960d76f0d1214ebf883dfb1a3ecc866fd38
Alex Kassil <contactalexkassil@gmail.com>
2017-04-29 15:55:18 -0700

    Added helper functions

--- chapter2/exercise2_19.scm
+++ chapter2/exercise2_19.scm
@@ -1,14 +1,18 @@
 ;;;; Count Change with lists
 
+;; United States Coins
 (define us-coins
   0(list 50 25 10 5 1))
 
+;; United Kingdom Coins
 (define uk-coins
-  (list 100 50 20 10 5 2 1 0.5))
+  (list 100 50 20 10 5 2 1 (/ 1 2)))
 
+;; Russia Coins
 (define ru-coins
   (list 1000 500 200 100 50 10 5 1))
 
+;; Count Change
 (define (cc amount coin-values)
   (cond ((= amount 0)
 	 1)
@@ -25,3 +29,16 @@
 		(first-denomination
 		 coin-values))
 	     coin-values)))))
+
+;;; Helper functions
+
+(define (no-more? coins)
+  (null? coins))
+
+(define (except-first-denomination coins)
+  (cdr coins))
+
+(define (first-denomination coins)
+  (car coins))
+
+
