dd27c64ec86f4bba77d5af15f13b72108d91cc72
Alex Kassil <contactalexkassil@gmail.com>
2017-04-30 18:31:17 -0700

    Same Parity function done

--- chapter2/exercise2_20.scm
+++ chapter2/exercise2_20.scm
@@ -4,6 +4,11 @@
   (or (and (even? x) (even? y)) (and (odd? x) (odd? y))))
 
 (define (same-parity x . i)
-  (display x)
-  (display i))
+  (define (same-parity-iter args return)
+    (cond ((null? args) return)
+	  ((same? x (car args)) 
+	   (same-parity-iter (cdr args) (append return (list (car args)))))
+	  (else (same-parity-iter (cdr args) return))))
+  (same-parity-iter i (list x)))
+
 
