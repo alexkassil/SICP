ab45e74d82af7889a2de6339c92e0e4a7136eba0
Alex Kassil <contactalexkassil@gmail.com>
2017-05-06 09:53:47 -0700

    Recursive Deep Reverse completed

--- chapter2/exercise2_27.scm
+++ chapter2/exercise2_27.scm
@@ -1,14 +1,27 @@
-;;;; Deep reverse
+;;;; Deep reverse recursively
 
+(define (r-deep-reverse l)
+  (cond ((null? l) '())
+	((not (pair? l)) l)
+	(else (append (r-deep-reverse (cdr l)) (list (r-deep-reverse (car l)))))))
+
+
+;;;; Reverse a list
+
+;; Reversing a list using iteration by appending last value to new list
 (define (reverse l)
-  (define (reverse-iter given new)
-    (display new)
-    (newline)
-    (cond ((null? given) new)
-	  ((not (pair? given)) (list given))
-	  (else (reverse-iter (cdr given)
-			      (append (reverse-iter (car given) new) new)))))
+  (define (reverse-iter l new)
+    (if (null? l)
+	new
+	(reverse-iter (cdr l) (append (list (car l)) new))))
   (reverse-iter l (list)))
+
+
+;; Reversing a list recursively
+
+(define (r-reverse l)
+  (cond ((null? l) '())
+	(else (append (r-reverse (cdr l)) (list (car l))))))
 
 
 
