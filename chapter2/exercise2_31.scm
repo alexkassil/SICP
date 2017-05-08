46482ac7b150a31946a40017d7f3c3c91ef5e302
Alex Kassil <contactalexkassil@gmail.com>
2017-05-07 18:48:24 -0700

    Add example of using tree map

--- chapter2/exercise2_31.scm
+++ chapter2/exercise2_31.scm
@@ -5,3 +5,7 @@
 	((not (pair? tree)) (procedure tree))
 	(else (cons (tree-map procedure (car tree))
 		    (tree-map procedure (cdr tree))))))
+
+;; Example
+(define (square-tree tree)
+  (tree-map square tree))
