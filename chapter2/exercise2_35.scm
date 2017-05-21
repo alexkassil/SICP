47a74064ed5ad1b49fcfc09b6577ccf1387658d1
Alex Kassil <contactalexkassil@gmail.com>
2017-05-20 17:04:36 -0700

    Finish exercise

--- chapter2/exercise2_35.scm
+++ chapter2/exercise2_35.scm
@@ -8,6 +8,26 @@
 		      initial
 		      (cdr sequence)))))
 
+(define (count-leaves t)
+  (accumulate
+   +
+   0
+   (map (lambda (x) 1)
+	(enumerate-tree t))))
 
+(define (enumerate-tree t)
+  (cond ((null? t) '())
+	((not (pair? t)) (list t))
+	(else (append (enumerate-tree (car t))
+		      (enumerate-tree (cdr t))))))
+
+
+;;; Count Leaves
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+	((not (pair? x)) 1)
+	(else (+ (count-leaves (car x))
+		 (count-leaves (cdr x))))))
 
 
