0d2fd2cbe619061370e84edeb61d32e15ca04245
Alex Kassil <contactalexkassil@gmail.com>
2017-07-23 15:47:26 -0700

    Delete material already in setbt.scm

--- chapter2/exercise2_64.scm
+++ chapter2/exercise2_64.scm
@@ -1,27 +1,5 @@
 ;;;; List -> Tree partial
 
-;;; Make and View Tree Elements
-
-(define (entry tree) (car tree))
-(define (left-branch tree) (cadr tree))
-(define (right-branch tree) (caddr tree))
-(define (make-tree entry left right)
-  (list entry left right))
-
-;;; Various Binary Tree Representations
-
-(define (element-of-set? x set)
-  (cond ((null? set) false)
-	((= x (entry set)) true)
-	((< x (entry set))
-	 (element-of-set?
-	  x
-	  (left-branch set)))
-	((> x (entry set))
-	 (element-of-set?
-	  x
-	  (left-branch set)))))
-
 (define (list->tree elements)
   (car (partial-tree
 	elements (length elements))))
