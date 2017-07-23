ad32832513949ef9aee7610f214bd2f5b16ad3db
Alex Kassil <contactalexkassil@gmail.com>
2017-07-23 12:36:37 -0700

    Binary Tree work

--- chapter2/setbt.scm
+++ chapter2/setbt.scm
@@ -31,21 +31,21 @@
 	  (adjoin-set x (right-branch set))))))
 ;;; Tree ways to represent the set {1, 3, 5, 7, 9, 11}
 
-(make-tree 7 
-	   (make-tree 3 
-		      (make-leaf 1) 
-		      (make-leaf 5)) 
-	   (make-tree 9 '() (make-leaf 11)))
+(define t1 (make-tree 7 
+		      (make-tree 3 
+				 (make-leaf 1) 
+				 (make-leaf 5)) 
+		      (make-tree 9 '() (make-leaf 11))))
 ;;     7
 ;;   /   \
 ;;  3     9
 ;; / \     \
 ;;1   5     11
 
-(make-tree 3 
-	   (make-leaf 1)
-	   (make-tree 7 (make-leaf 5)
-		      (make-tree 9 '() (make-leaf 11))))
+(define t2 (make-tree 3 
+		      (make-leaf 1)
+		      (make-tree 7 (make-leaf 5)
+				 (make-tree 9 '() (make-leaf 11)))))
 ;;   3
 ;;  / \
 ;; 1   7
@@ -54,9 +54,9 @@
 ;;        \
 ;;        11
 
-(make-tree 5 (make-tree 3 
-			(make-leaf 1) '()) 
-	   (make-tree 9 (make-leaf 7) (make-leaf 11)))
+(define t3 (make-tree 5 (make-tree 3 
+				   (make-leaf 1) '()) 
+		      (make-tree 9 (make-leaf 7) (make-leaf 11))))
 ;;    5
 ;;   / \
 ;;  3   9
@@ -64,8 +64,15 @@
 ;;1   7  11
 
 ;;; Unbalanced tree 1-4 list
-(make-tree 1 '() (make-tree 2 '() 
-			    (make-tree 3 '() (make-tree 4 '() '())))) 
-;; 1-2-3-4
+(define t4 (make-tree 1 '() (make-tree 2 '() 
+				       (make-tree 3 '() (make-tree 4 '() '())))) )
+;; 1
+;;  \
+;;   2
+;;    \
+;;     3
+;;      \
+;;       4
+
 
 
