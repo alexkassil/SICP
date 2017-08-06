2aa5e65054eb6a38d2a36a66bdec95e49b8289fb
Alex Kassil <alexkassil@sbcglobal.net>
2017-08-05 22:33:28 -0700

    Add sicp ex 1.32a

--- hws/week02/sicp_exercises.scm
+++ hws/week02/sicp_exercises.scm
@@ -20,3 +20,23 @@
    2
    (lambda (x) (+ 2 x))
    10000))
+
+;;; 1.32 (a)
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+	  null-value
+	  (combiner (term a)
+				(accumulate combiner
+							null-value
+							term
+							(next a)
+							next
+							b))))
+
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+
+
+(define (product term a next b)
+  (accumulate * 1 term a next b))
