699e33fd84943f283779f62a2591cb0332c94e4d
Alex Kassil <alexkassil@sbcglobal.net>
2017-08-05 22:43:46 -0700

    Add sicp ex 1.33

--- hws/week02/sicp_exercises.scm
+++ hws/week02/sicp_exercises.scm
@@ -40,3 +40,38 @@
 
 (define (product term a next b)
   (accumulate * 1 term a next b))
+
+;;; 1.33
+
+(define (filter-accumulate filter combiner null-value term a next b)
+  (if (> a b)
+	  null-value
+	  (combiner (if (filter a) (term a) null-value)
+				(filter-accumulate filter
+								   combiner
+								   null-value
+								   term
+								   (next a)
+								   next
+								   b))))
+
+(define (sum-of-squares-primes a b)
+  (filter-accumulate prime?
+					 +
+					 0
+					 (lambda (x) (* x x))
+					 a
+					 (lambda (x) (+ 1 x))
+					 b))
+
+(define (prod-<n-relative-prime n)
+  (filter-accumulate (lambda (x)
+					   (and (equals? (gcd x n) 1)
+							(not (equals? n x))))
+					 *
+					 1
+					 (lambda (x) x)
+					 1
+					 (lambda (x) (+ x 1))
+					 n))
+					 
