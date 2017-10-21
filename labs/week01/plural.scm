975d7aa6d71a443fac26afee07408442f15d2f6e
Alex Kassil <alexkassil@sbcglobal.net>
2017-10-21 14:52:32 -0700

    Recreate plural'

--- labs/week01/plural.scm
+++ labs/week01/plural.scm
@@ -5,10 +5,13 @@
 
 (define (plural wd)
   (cond ((equal? (last wd) 'y)
-		 (cond ((vowel? (last (bl wd))) (word wd 's))
-			   (else (word (bl wd) 'ies))))
-		((equal? (last wd) 'x) (word wd 'es))
+		 (if (vowel? (last (bl wd)))
+			 (word wd 's)
+			 (word (bl wd) 'ies)))
+		((equal? (last wd) 'x)
+		 (word wd 'es))
 		(else (word wd 's))))
 
 (define (vowel? letter)
-  (member? letter 'aeiou))
+  (member? letter '(a e i o u)))
+		  
