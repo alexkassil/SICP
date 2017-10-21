bc7c5dd335918442e607e7ae008deb1bb349dc76
Alex Kassil <alexkassil@sbcglobal.net>
2017-10-21 15:15:24 -0700

    Create substitute

---
+++ labs/week01/substitute.scm
@@ -0,0 +1,5 @@
+(define (substitute sent old new)
+  (cond ((empty? sent) '())
+		((equal? (first sent) old)
+		 (se new (substitute (bf sent) old new)))
+		(else (se (first sent) (substitute (bf sent) old new)))))
