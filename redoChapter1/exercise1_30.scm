140a88bee60ac89ca78699f28b75c8b18df52e0c
Alex Kassil <alexkassil@sbcglobal.net>
2020-07-06 23:28:26 -0700

    tail recursive sum

---
+++ redoChapter1/exercise1_30.scm
@@ -0,0 +1,16 @@
+(define (sequence-sum term next a b)
+  (if (> a b)
+	  0
+	  (+ (term a)
+		 (sequence-sum term next (next a) b))))
+
+(define (sum term next a b)
+  (define (iter a result)
+    (if (> a b)
+	result
+	(iter (next a) (+ (term a) result))))
+  (iter a 0))
+
+(sum (lambda (x) (* x x x)) (lambda (x) (+ x 1)) 0 10)
+
+(sum (lambda (x) (/ 8.0 (* x (+ x 2)))) (lambda (x) (+ x 4)) 1 10000000)
