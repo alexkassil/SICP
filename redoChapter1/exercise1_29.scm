46127b2261898dc6644ee509ac7ced2498c0804e
Alex Kassil <alexkassil@sbcglobal.net>
2020-07-06 23:23:25 -0700

    Add updates to ex 29

--- redoChapter1/exercise1_29.scm
+++ redoChapter1/exercise1_29.scm
@@ -4,4 +4,18 @@
 	  (+ (term a)
 		 (sequence-sum term next (next a) b))))
 
-(
+(define (cube x) (* x x x))
+
+(define (simpsons-rule f a b n)
+  (define h (/ (- b a) n))
+  (define (y k)
+    (f (+ a (* k h))))
+  (define (helper k)
+    (cond ((= k n) (y n))
+	  ((= (remainder k 2) 1) (+ (* 4 (y k)) (helper (+ k 1))))
+	  (else (+ (* 2 (y k)) (helper (+ k 1))))))
+  (* (/ h 3.0) (+ (y 0) (helper 1))))
+
+(simpsons-rule cube 0 1 100)
+
+(simpsons-rule cube 0 1 1000)
